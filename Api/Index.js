c2d15893bdb9e26090ab59df4f53a7cb2097d41a
Memo <guillermo.correamo@gmail.com>
2022-06-10 19:59:52 -0400

    master

--- Api/Index.js
+++ Api/Index.js
@@ -5,12 +5,24 @@ const cassandra = require('cassandra-driver');
 
 const app = express();
 
-//Crear cliente de Cassandra con 3 nodos
 const client = new cassandra.Client({
-    contactPoints: ['cassandra-cluster:9042', 'cassandra-cluster:9142', 'cassandra-cluster:9242'],
+    contactPoints: ['cassandra-cluster:9042', 'cassandra-cluster:9042', 'cassandra-cluster:9042'],
     localDataCenter: 'datacenter1',
-    keyspace: 'test'
+    keyspace: 'cassandra_keyspace',
+    queryOptions: {
+        consistency: cassandra.types.consistencies.one
+    },
+    authProvider: new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra')
 });
+
+client.connect((err) => {
+    if (err) {
+        console.log('Error al conectar con Cassandra');
+    } else {
+        console.log('Conectado con Cassandra');
+    }
+}
+);
 
 app.use(
     bodyParser.urlencoded({
@@ -27,150 +39,146 @@ var host = process.env.PORT || '0.0.0.0';
 
 
 
-app.post("/create", (req, res) => {
-  console.log("Creando receta");
-  var nombre = req.body.nombre;
-  var apellido = req.body.apellido;
-  var rut = req.body.rut;
-  var email = req.body.email;
-  var fecha_nacimiento = req.body.fecha_nacimiento;
-  var comentario = req.body.comentario;
-  var farmacos = req.body.farmacos;
-  var doctor = req.body.doctor;
-  //Si el paciente no existe, debera crearlo junto a la receta unica.
-  client.execute(`SELECT * FROM paciente WHERE rut = ${rut}`, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error");
-    } else {
-      if (result.rows.length == 0) {
-        client.execute(
-          //Se inserta el paciente y la receta en distintas tablas.
-          `INSERT INTO paciente (rut, nombre, apellido, email, fecha_nacimiento) VALUES (${rut}, '${nombre}', '${apellido}', '${email}', '${fecha_nacimiento}')`,
-          (err, result) => {
-            if (err) {
-              console.log(err);
-              res.send("Error");
-            } else {
-              console.log("Paciente creado");
-              client.execute(
-                //Inserta receta en base al id del paciente
-                `INSERT INTO receta (id_paciente, comentario, farmacos, doctor) VALUES (${rut}, '${comentario}', '${farmacos}', '${doctor}')`,
-                (err, result) => {
-                  if (err) {
-                    console.log(err);
-                    res.send("Error");
-                  } else {
-                    console.log("Receta creada");
-                    res.send("Receta creada");
-                  }
-                }
-              );
-            }
-          }
-        );
-      } else {
-        console.log("Paciente ya existe");
-        client.execute(
-          //Inserta receta en base al id del paciente
-          `INSERT INTO receta (id_paciente, comentario, farmacos, doctor) VALUES (${rut}, '${comentario}', '${farmacos}', '${doctor}')`,
-          (err, result) => {
-            if (err) {
-              console.log(err);
-              res.send("Error");
-            } else {
-              console.log("Receta creada");
-              res.send("Receta creada");
-            }
-          }
-        );
-      }
-    }
-  }
-  );
-});
-app.post("/edit", (req, res) => {
-  console.log("Editando receta");
-  var id = req.body.id;
-  var comentario = req.body.comentario;
-  var farmacos = req.body.farmacos;
-  var doctor = req.body.doctor;
-  //Este metodo debera enviar los datos a editar de la receta, siendo necesario el id de este  ́ultimo
-  client.execute(
-    `UPDATE receta SET comentario = '${comentario}', farmacos = '${farmacos}', doctor = '${doctor}' WHERE  id_paciente = ${id}`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.send("Error");
-      } else {
-        res.send("Paciente editado");
-      }
-    }
-  );
-});
+// app.post("/create", (req, res) => {
+//   console.log("Creando receta");
+//   var nombre = req.body.nombre;
+//   var apellido = req.body.apellido;
+//   var rut = req.body.rut;
+//   var email = req.body.email;
+//   var fecha_nacimiento = req.body.fecha_nacimiento;
+//   var comentario = req.body.comentario;
+//   var farmacos = req.body.farmacos;
+//   var doctor = req.body.doctor;
+//   //Si el paciente no existe, debera crearlo junto a la receta unica.
+//   client.execute(`SELECT * FROM paciente WHERE rut = ${rut}`, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       res.send("Error");
+//     } else {
+//       if (result.rows.length == 0) {
+//         client.execute(
+//           //Se inserta el paciente y la receta en distintas tablas.
+//           `INSERT INTO paciente (rut, nombre, apellido, email, fecha_nacimiento) VALUES (${rut}, '${nombre}', '${apellido}', '${email}', '${fecha_nacimiento}')`,
+//           (err, result) => {
+//             if (err) {
+//               console.log(err);
+//               res.send("Error");
+//             } else {
+//               console.log("Paciente creado");
+//               client.execute(
+//                 //Inserta receta en base al id del paciente
+//                 `INSERT INTO receta (id_paciente, comentario, farmacos, doctor) VALUES (${rut}, '${comentario}', '${farmacos}', '${doctor}')`,
+//                 (err, result) => {
+//                   if (err) {
+//                     console.log(err);
+//                     res.send("Error");
+//                   } else {
+//                     console.log("Receta creada");
+//                     res.send("Receta creada");
+//                   }
+//                 }
+//               );
+//             }
+//           }
+//         );
+//       } else {
+//         console.log("Paciente ya existe");
+//         client.execute(
+//           //Inserta receta en base al id del paciente
+//           `INSERT INTO receta (id_paciente, comentario, farmacos, doctor) VALUES (${rut}, '${comentario}', '${farmacos}', '${doctor}')`,
+//           (err, result) => {
+//             if (err) {
+//               console.log(err);
+//               res.send("Error");
+//             } else {
+//               console.log("Receta creada");
+//               res.send("Receta creada");
+//             }
+//           }
+//         );
+//       }
+//     }
+//   }
+//   );
+// });
+// app.post("/edit", (req, res) => {
+//   console.log("Editando receta");
+//   var id = req.body.id;
+//   var comentario = req.body.comentario;
+//   var farmacos = req.body.farmacos;
+//   var doctor = req.body.doctor;
+//   //Este metodo debera enviar los datos a editar de la receta, siendo necesario el id de este  ́ultimo
+//   client.execute(
+//     `UPDATE receta SET comentario = '${comentario}', farmacos = '${farmacos}', doctor = '${doctor}' WHERE  id_paciente = ${id}`,
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//         res.send("Error");
+//       } else {
+//         res.send("Paciente editado");
+//       }
+//     }
+//   );
+// });
 
 
-app.post("/delete", (req, res) => {
-  console.log("Eliminando receta");
-  var id = req.body.id;
-  //Se elimina receta
-  client.execute(
-    `DELETE FROM receta WHERE id = ${id}`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.send("Error");
-      } else {
-        res.send("Receta eliminada");
-      }
-    }
-  );
-});
+// app.post("/delete", (req, res) => {
+//   console.log("Eliminando receta");
+//   var id = req.body.id;
+//   //Se elimina receta
+//   client.execute(
+//     `DELETE FROM receta WHERE id = ${id}`,
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//         res.send("Error");
+//       } else {
+//         res.send("Receta eliminada");
+//       }
+//     }
+//   );
+// });
 
 app.get("/", (req, res) => {
   res.send("Y aqui estamos de vuelta!!!");
-  //crear tabla paciente y receta
-  client.execute(
-    `CREATE TABLE IF NOT EXISTS paciente (
-      rut int,
-      nombre text,
-      apellido text,
-      email text,
-      fecha_nacimiento text,
-      PRIMARY KEY (rut)
-    )`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.send("Error");
-
-      } else {
-        console.log("Tabla paciente creada");
-        client.execute(
-          `CREATE TABLE IF NOT EXISTS receta (
-            id int,
-            id_paciente int,
-            comentario text,
-            farmacos text,
-            doctor text,
-            PRIMARY KEY (id)
-          )`,
-          (err, result) => {
-            if (err) {
-              console.log(err);
-              res.send("Error");
-            } else {
-              console.log("Tabla receta creada");
-              res.send("Tablas creadas");
-            }
-          }
-        );
-      }
-    }
-  );  
 });
 
   //Se crea el puerto
   app.listen(port,host, () => {
     console.log(`API run in: http://localhost:${port}.`);
+    // client.execute(
+    //   `CREATE TABLE IF NOT EXISTS paciente (
+    //     rut int,
+    //     nombre text,
+    //     apellido text,
+    //     email text,
+    //     fecha_nacimiento text,
+    //     PRIMARY KEY (rut)
+    //   )`,
+    //   (err, result) => {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log("Tabla paciente creada");
+    //       client.execute(
+    //         `CREATE TABLE IF NOT EXISTS receta (
+    //           id int,
+    //           id_paciente int,
+    //           comentario text,
+    //           farmacos text,
+    //           doctor text,
+    //           PRIMARY KEY (id)
+    //         )`,
+    //         (err, result) => {
+
+    //           if (err) {
+    //             console.log(err);
+    //           } else {
+    //             console.log("Tabla receta creada");
+    //           }
+    //         }
+    //       );
+    //     }
+    //   }
+    // );
   });
